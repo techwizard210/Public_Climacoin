b04532644fd92dc2f6461c9d63d4c07f082258f4
Worlddev5007 <worlddev5007@gmail.com>
2022-07-10 22:01:18 +0900

    Refactored the balances pallet

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -328,20 +328,6 @@ impl pallet_grandpa::Config for Runtime {
 	type MaxAuthorities = ConstU32<32>;
 }
 
-impl pallet_balances::Config for Runtime {
-	type MaxLocks = ConstU32<50>;
-	type MaxReserves = ();
-	type ReserveIdentifier = [u8; 8];
-	/// The type for recording an account's balance.
-	type Balance = Balance;
-	/// The ubiquitous event type.
-	type Event = Event;
-	type DustRemoval = ();
-	type ExistentialDeposit = ConstU128<500>;
-	type AccountStore = System;
-	type WeightInfo = pallet_balances::weights::SubstrateWeight<Runtime>;
-}
-
 parameter_types! {
 	pub const TransactionByteFee: Balance = 1;
 }
